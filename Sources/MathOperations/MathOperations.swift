ccf7d870d1aac9fc71ab56787f3aac3950770dbf
Samarth Kejriwal <samarth.kejriwal@caastle.com>
2020-04-29 16:48:48 +0530

    Added a function to add numbers of an array

--- Sources/MathOperations/MathOperations.swift
+++ Sources/MathOperations/MathOperations.swift
@@ -30,3 +30,14 @@
 
     return sum
 }
+
+@discardableResult public func addArrayNumbers(numbers: [String]) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum+= Int(number) ?? 0
+    }
+    
+    print("adding the numbers gives: \(sum)")
+    
+    return sum
+}
